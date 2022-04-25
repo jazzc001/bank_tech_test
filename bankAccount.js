06524bd4fcfc777314e014339bc88536567eb1be
jazzc001 <jazzcheung212@gmail.com>
2022-04-25 16:32:12 +0100

    deleted bank statement class.  put the printing method in the bank account class

--- bankAccount.js
+++ bankAccount.js
@@ -2,17 +2,21 @@ class BankAccount {
 
     constructor() {
         this.balance = 0;
+        this.#transaction = [];
     }
 
     deposit(money) {
-        return this.balance += money;
+        this.balance += money;
+        this.transaction.push({date: new Date(), credit: 0, debit: money, balance: this.balance});
+        
     }
 
     withdrawal(money) {
-        return this.balance -= money;
+        this.balance -= money;
+        this.transaction.push({date: new Date(), credit: money, debit: 0, balance: this.balance});
     }
 
-
+    print()
 }
 
 module.exports = BankAccount;
