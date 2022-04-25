118f2a385b1db44caf0956faa46c2c15fcc3bf43
jazzc001 <jazzcheung212@gmail.com>
2022-04-25 12:49:58 +0100

    added another tech test of checking initialzed balance equals to 0.  made a method of deposit inside a bankaccount class with balance initially as 0

--- bankAccount.js
+++ bankAccount.js
@@ -1,3 +1,14 @@
 class BankAccount {
-    
+
+    constructor() {
+        this.balance = 0;
+    }
+
+    deposit(money) {
+        return this.balance += money;
+    }
+
+
 }
+
+module.exports = BankAccount;
--- bankAccount.test.js
+++ bankAccount.test.js
@@ -1,17 +1,19 @@
 const BankAccount = require('./bankAccount')
 
 describe('bankAccount', ()=> {
+    let account;
+    beforeEach(()=> {
+        account = new BankAccount();
+    })
 
-    describe('BankAccount model', ()=>{
-        let account;
-        beforeEach(()=> {
-            account = new BankAccount();
-        })
+    test('initial balance is zero', ()=>{
+       expect(account.balance).toEqual(0)
+        
+    })
 
-        describe('.deposit', () => {
-            it('make deposit of 100', () => {
-                expect(account.deposit(100)).toEqual(100)
-            })
+    describe('.deposit', () => {
+        test('make deposit of 100, balance equals to 100', () => {
+            expect(account.deposit(100)).toEqual(100)
         })
     })
 
